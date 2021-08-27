bb5da40adbe7ec4f67b89305ca5b2edf2d6aa608
Ziemas <ziemas@ziemas.se>
2021-08-27 19:48:03 +0200

    Add function for loading palettes in advance
    And make sprites get palette by string

--- common/shaderglobals.go
+++ common/shaderglobals.go
@@ -2,8 +2,15 @@ package common
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
+type PalTex struct {
+	Texture rl.Texture2D
+	Data []byte
+	Init bool
+}
+
 //TODO: Yeah yeah, move this out
 var (
 	PaletteShader    rl.Shader
 	PaletteShaderLoc int32
+	PaletteTexture   map[string]*PalTex
 )
